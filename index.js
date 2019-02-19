7e0f72d12d31182f75d6da1ace1ba93d970729a6
Cully Curwen <c.t.g.curwen@gmail.com>
2019-02-19 17:57:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var array = []
+  let array = []
   for (let i = 0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  let array = []
+  let i = 0
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`)
+    i++
   }
   return array
 }
